5ba791ebf7311acecb5d1bb79dd5c06cc2c54d09
Muhammad Iqbal <iqbal.0809@gmail.com>
2017-02-08 06:57:00 +0700

    Delete keys.js

--- server/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "consumer_key":"YXywtIpx54Ev5izDoXEz4xNG2",
-  "consumer_secret":"E8f07JNRKpBVzwtAknwuwHnWKhZlhaAeAfOLIIKPLYib1qD23C",
-  "access_token_key":"105022050-FPWd6Pe23xy4aDB5yvikshIDvybY5YkemA9YNWtE",
-  "access_token_secret":"kbk9iC89vA5f0pT8Z3sUxElKWIn57sB6bnDSTJ4Z2ckDV"
-}
